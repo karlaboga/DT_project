eba231398f964696643853a5ccbcd9b8a9dce3f8
karlaboga <karlamaria.boga@gmail.com>
2026-06-02 14:19:22 +0300

    MERGE

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       },
     },
